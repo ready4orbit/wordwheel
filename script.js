b0466fb5016f4318a8f99f3d252c02f1e5b7285d
ready4orbit <106458252+ready4orbit@users.noreply.github.com>
2022-05-28 20:21:31 -0700

    Add files via upload

--- script.js
+++ script.js
@@ -558,18 +558,16 @@ document.getElementById("keyboard-cont").addEventListener("click", (e) => {
 let touchstartX = 0
 let touchendX = 0
 
-const slider = document.getElementById('slider')
-
 function handleGesture() {
-  if (touchendX < touchstartX) alert('swiped left!')
-  if (touchendX > touchstartX) alert('swiped right!')
+  if (touchendX < touchstartX) nextWord()
+  if (touchendX > touchstartX) prevWord()
 }
 
-slider.addEventListener('touchstart', e => {
+document.addEventListener('touchstart', e => {
   touchstartX = e.changedTouches[0].screenX
 })
 
-slider.addEventListener('touchend', e => {
+document.addEventListener('touchend', e => {
   touchendX = e.changedTouches[0].screenX
   handleGesture()
 })
