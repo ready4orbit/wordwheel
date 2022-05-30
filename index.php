eb7ce6d80e319617b6825622255e54483f1869eb
ready4orbit <106458252+ready4orbit@users.noreply.github.com>
2022-05-30 14:48:02 -0700

    Add files via upload

--- index.php
+++ index.php
@@ -6,7 +6,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>WordWheel</title>
     
-    <link rel="stylesheet" href="style2.css">
+    <link rel="stylesheet" href="style.css">
     
     <link
     rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/animate.css/4.1.1/animate.min.css"/>
@@ -65,6 +65,6 @@
     src="https://code.jquery.com/jquery-3.6.0.min.js"
     integrity="sha256-/xUj+3OJU5yExlq6GSYGSHk7tPXikynS7ogEvDej/m4="
     crossorigin="anonymous"></script>
-    <script src="script2.js" type="module"></script>
+    <script src="script.js" type="module"></script>
 </body>
 </html>
