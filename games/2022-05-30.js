442273bc817e5297bcc85a00270a9af751b5494d
ready4orbit <106458252+ready4orbit@users.noreply.github.com>
2022-06-04 19:48:07 -0700

    Add files via upload

---
+++ games/2022-05-30.js
@@ -0,0 +1,17 @@
+const WORDS = [
+    'yell',
+    'leech',
+    'headline',
+    'ending',
+    'groggy',
+    'yesterday',
+  ]
+
+const CLUES = [
+    'Shout',
+    'Parasite',
+    'Something you might click',
+    'Subject of a \"spoiler alert\"',
+    'Operating on little sleep, say',
+    'It\'s just a memory',
+  ]
---
+++ games/2022-06-04.js
@@ -0,0 +1,17 @@
+const WORDS = [
+    'loud',
+    'drain',
+    'night',
+    'total',
+    'lamb',
+    'bagel',
+  ]
+
+const CLUES = [
+    'Noisy',
+    'Empty of liquid',
+    'When nocturnal animals stir',
+    'Line on a receipt',
+    'Sacrificial ____',
+    '\"Everything,\" for one',
+  ]
