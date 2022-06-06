f6585ecf62a9f4f79e1b54273a5e322cdd1978b9
ready4orbit <106458252+ready4orbit@users.noreply.github.com>
2022-06-06 15:14:26 -0700

    Add files via upload

---
+++ games/2022-06-10.js
@@ -0,0 +1,17 @@
+const WORDS = [
+    'chef',
+    'fetch',
+    'hiccup',
+    'probe',
+    'easel',
+    'lyric'
+  ]
+
+const CLUES = [
+    'Kitchen captain',
+    'Common dog command',
+    'Small issue',
+    'Question',
+    'Supporter of the arts?',
+    'Sentence you sing'
+  ]
