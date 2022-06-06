ce5a038870625649ebdbc3c48d33994a5c5768b5
ready4orbit <106458252+ready4orbit@users.noreply.github.com>
2022-06-06 09:29:47 -0700

    Add files via upload

---
+++ games/2022-06-08.js
@@ -0,0 +1,17 @@
+const WORDS = [
+    'pastry',
+    'yolk',
+    'kazoo',
+    'open',
+    'napkin',
+    'nonstop',
+  ]
+
+const CLUES = [
+    'Eclair or scone',
+    'The \"sunny side\" of \"sunny sideup\"',
+    'Instrument you need no hands for',
+    'Available',
+    'Part of a table setting',
+    'Without end',
+  ]
---
+++ games/2022-06-09.js
@@ -0,0 +1,17 @@
+const WORDS = [
+    'feel',
+    'loop',
+    'plaid',
+    'dinosaur',
+    'rotate',
+    'engulf',
+  ]
+
+const CLUES = [
+    'Sense',
+    'Scary bit of a rollercoaster',
+    'Like many kilts',
+    '\"Terrible lizard\" in Greek',
+    'Change out',
+    'Consume',
+  ]
