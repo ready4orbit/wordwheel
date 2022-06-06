ba49136dbcb74687830064dac2ba59abe3162dc3
ready4orbit <106458252+ready4orbit@users.noreply.github.com>
2022-06-05 20:31:49 -0700

    Add files via upload

---
+++ games/2022-06-06.js
@@ -0,0 +1,17 @@
+const WORDS = [
+    'theif',
+    'fog',
+    'garden',
+    'nomad',
+    'deep',
+    'portrait',
+  ]
+
+const CLUES = [
+    'Robber',
+    'Driving hazard',
+    'Home for a gnome, perhaps',
+    'Digital _____',
+    'Profound',
+    'You might sit for one',
+  ]
---
+++ games/2022-06-07.js
@@ -0,0 +1,17 @@
+const WORDS = [
+    'meat',
+    'trap',
+    'plum',
+    'might',
+    'threat',
+    'tourism',
+  ]
+
+const CLUES = [
+    'What vegetarians don\'t eat',
+    'Ensnare',
+    'A color and flavor',
+    'Strength',
+    'One might be veiled',
+    '21% of Hawaii\'s economy',
+  ]
